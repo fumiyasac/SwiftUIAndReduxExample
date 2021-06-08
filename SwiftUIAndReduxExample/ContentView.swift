014b79fbc5c7c49a4b3451f79cd5b7175e175705
Fumiya Sakai <just1factory@gmail.com>
2021-06-09 05:43:24 +0900

    initial commit by Xcode13

---
+++ SwiftUIAndReduxExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2021/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAndReduxExampleApp.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAndReduxExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
