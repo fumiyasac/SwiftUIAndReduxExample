30ca57f6ea36bb3b8c1a9135deee04bb3b37495d
Fumiya Sakai <just1factory@gmail.com>
2021-10-23 21:57:08 +0900

    簡単にSwaggerでの出力までを定義

--- Backend/swiftui_and_redux_example_backend/build.gradle.kts
+++ Backend/swiftui_and_redux_example_backend/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("io.springfox:springfox-swagger2:2.9.2")
+	implementation ("io.springfox:springfox-swagger-ui:2.9.2")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- Backend/swiftui_and_redux_example_backend/src/main/kotlin/net/just1factory/swiftui_and_redux_example_backend/controller/api/v1/SampleController.kt
+++ Backend/swiftui_and_redux_example_backend/src/main/kotlin/net/just1factory/swiftui_and_redux_example_backend/controller/api/v1/SampleController.kt
@@ -4,15 +4,18 @@ package net.just1factory.swiftui_and_redux_example_backend.controller.api.v1
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestParam
 
 // MEMO: Swagger UIに記載する内容を表示するためのアノテーション
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(description = "エンドポイント")
 class SampleController {
 
-	@GetMapping("/")
+	@ApiOperation(value = "サンプル", produces = "application/json", notes = "Hello worldを表示する", response = String::class)
+	@GetMapping("/sample")
 	fun get(): String {
 		return "Hello world"
 	}
