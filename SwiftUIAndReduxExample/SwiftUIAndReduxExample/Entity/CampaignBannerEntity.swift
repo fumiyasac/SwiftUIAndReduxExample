2c3a11e9b71959c484eed11ae11de7401005092e
fumiyasac <just1factory@gmail.com>
2022-10-02 23:48:25 +0900

    Entitiyを新しく追加（TOPバナー表示部分）

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/Entity/CampaignBannerEntity.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Entity/CampaignBannerEntity.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+struct CampaignBannerEntity: Hashable, Decodable {
+
+    let id: Int
+    let bannerContentsId: Int
+    let bannerUrl: String
+    let title: String
+    let caption: String
+    let announcementAt: String
+
+    // MARK: - Enum
+
+    private enum Keys: String, CodingKey {
+        case id
+        case bannerContentsId
+        case bannerUrl
+        case title
+        case caption
+        case announcementAt
+    }
+
+    // MARK: - Initializer
+
+    init(from decoder: Decoder) throws {
+
+        // JSONの配列内の要素を取得する
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        // JSONの配列内の要素にある値をDecodeして初期化する
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.bannerContentsId = try container.decode(Int.self, forKey: .bannerContentsId)
+        self.bannerUrl = try container.decode(String.self, forKey: .bannerUrl)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.caption = try container.decode(String.self, forKey: .caption)
+        self.announcementAt = try container.decode(String.self, forKey: .announcementAt)
+    }
+
+    // MARK: - Hashable
+
+    // MEMO: Hashableプロトコルに適合させるための処理
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: CampaignBannerEntity, rhs: CampaignBannerEntity) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
