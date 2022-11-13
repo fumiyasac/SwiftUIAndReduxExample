b2b778986643303a61462fef201527088d6218d3
fumiyasac <just1factory@gmail.com>
2022-11-13 20:44:55 +0900

    Realm登録用のEntityの新規作成

---
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Model/RealmObject/FavoriteRealmEntity.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteRealmEntity.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2022/11/13.
+//
+
+import Foundation
+import RealmSwift
+
+final class FavoriteRealmEntity: Object {
+
+    // TODO: 検索データ用のColumn定義を決めること
+}
