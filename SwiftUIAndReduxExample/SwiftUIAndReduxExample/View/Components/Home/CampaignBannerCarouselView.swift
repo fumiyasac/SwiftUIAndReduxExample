8fc6633be2441eec1d02a645d20e108339512fd3
fumiyasac <just1factory@gmail.com>
2022-12-07 06:55:00 +0900

    SnapCarousel形式の表示セクション要素を表示する

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Home/CampaignBannerCarouselView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Home/CampaignBannerCarouselView.swift
@@ -6,21 +6,186 @@
 //
 
 import SwiftUI
+import Kingfisher
+
+// MEMO: Snapを伴うCarousel方式でのバナー表示の参考
+// https://stackoverflow.com/questions/72343827/carousel-view-swiftui
 
 struct CampaignBannerCarouselView: View {
-    var body: some View {
 
-        // TODO: キャンペーンバナー用のCarousel表示Viewを構築する
-        Text("てすと")
+    // MARK: - Property
+
+    private let screen = UIScreen.main.bounds
+
+    private var carouselWidth: CGFloat {
+        return screen.width - 32.0
+    }
+
+    private var carouselHeight: CGFloat {
+        return carouselWidth * 156 / 375
+    }
+
+    // MEMO: Carousel表示時に利用する変化量を格納するための変数
+    @State private var snappedOffset: CGFloat
+    @State private var draggingOffset: CGFloat
+
+    // MEMO: Carouselに表示する内容を格納するための変数
+    @State private var campaignBannersCarouselViewObjects: [CampaignBannerCarouselViewObject] = []
+
+    // MARK: - Initializer
+    
+    init(campaignBannersCarouselViewObjects: [CampaignBannerCarouselViewObject]) {
+        self.campaignBannersCarouselViewObjects = campaignBannersCarouselViewObjects
+
+        // イニシャライザ内で「_(変数名)」値を代入することでState値の初期化を実行する
+        _snappedOffset = State(initialValue: 0.0)
+        _draggingOffset = State(initialValue: 0.0)
+    }
+
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack {
+            ForEach(campaignBannersCarouselViewObjects) { viewObject in
+                ZStack {
+                    KFImage(viewObject.bannerUrl)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(12.0)
+                }
+                .frame(width: carouselWidth, height: carouselHeight)
+                .scaleEffect(getCarouselElementModifierValueBy(itemId: viewObject.id, ratio: 0.2))
+                .opacity(getCarouselElementModifierValueBy(itemId: viewObject.id, ratio: 0.3))
+                .zIndex(getCarouselElementModifierValueBy(itemId: viewObject.id, ratio: 0.1))
+                .offset(x: calculateHorizontalOffsetBy(itemId: viewObject.id), y: 0.0)
+                .contentShape(Rectangle())
+                .onTapGesture(perform: {
+                    print("想定: Tap処理を実行した際に何らかの処理を実行する (ID:\(viewObject.id))")
+                })
+            }
+        }
+        .modifier(
+            // MEMO: DragGestureの値変化を利用したCarousel表示用のModifier定義
+            // 👉 State値に$をつけて値変化と連動させている点がポイントになる
+            CampaignBannerCarouselViewModifier(
+                snappedOffset: $snappedOffset,
+                draggingOffset: $draggingOffset,
+                viewObjectsCount: campaignBannersCarouselViewObjects.count
+            )
+        )
+    }
+
+    // MARK: - Private Function
+
+    private func getCarouselElementModifierValueBy(itemId: Int, ratio: CGFloat) -> CGFloat {
+        // MEMO: scaleEffect / opacity / zIndexに必要な調整値を算出する
+        let distanceByItemId = calculateDistanceBy(itemId: itemId)
+        return 1.0 - abs(distanceByItemId) * ratio
+    }
+
+    private func calculateDistanceBy(itemId: Int) -> CGFloat {
+        // MEMO: remainderを利用して間隔値を算出する
+        // 参考: https://dev.classmethod.jp/articles/utility-extension-remainder/
+        let allItemsCount = campaignBannersCarouselViewObjects.count
+        let draggingOffsetByItemId = (draggingOffset - CGFloat(itemId))
+        return draggingOffsetByItemId
+            .remainder(dividingBy: CGFloat(allItemsCount))
+    }
+
+    private func calculateHorizontalOffsetBy(itemId: Int) -> CGFloat {
+        // MEMO: 三角関数(この場合はsinθ)を利用して角度を算出し、奥行きのある重なりを表現する
+        let allItemsCount = campaignBannersCarouselViewObjects.count
+        let angle = Double.pi * 2 / Double(allItemsCount) * calculateDistanceBy(itemId: itemId)
+        return sin(angle) * 200
     }
 }
+
+// MARK: - Modifier
+
+struct CampaignBannerCarouselViewModifier: ViewModifier {
+
+    // MARK: - Property
+
+    // MEMO: CampaignBannerCarouselViewに定義したState値の変化量と連動する形としている
+    @Binding private var snappedOffset: CGFloat
+    @Binding private var draggingOffset: CGFloat
+
+    private var viewObjectsCount: Int
+
+    // MARK: - Initializer
+
+    init(
+        snappedOffset: Binding<CGFloat>,
+        draggingOffset: Binding<CGFloat>,
+        viewObjectsCount: Int
+    ) {
+        // イニシャライザ内で「_(変数名)」値を代入することでBinding値の初期化を実行する
+        // ※viewObjectの総数については従来通りの初期化処理で良い
+        _snappedOffset = snappedOffset
+        _draggingOffset = draggingOffset
+        self.viewObjectsCount = viewObjectsCount
+    }
+
+    // MARK: - body
+
+    func body(content: Content) -> some View {
+        content
+            .gesture(
+                DragGesture()
+                .onChanged({ value in
+                    // 👉 Carousel要素の移動中はStateと連動するdraggingOffset値を更新する
+                    draggingOffset = snappedOffset + value.translation.width / 250
+                })
+                .onEnded({ value in
+                    // 👉 Carousel要素の移動終了時は自然に元の位置または動かそうとした位置に戻る様にしている
+                    withAnimation {
+                        draggingOffset = snappedOffset + value.translation.width / 250
+                        draggingOffset = round(draggingOffset).remainder(dividingBy: Double(viewObjectsCount))
+                        snappedOffset = draggingOffset
+                    }
+                })
+            )
+    }
+}
+
+// MARK: - ViewObject
+
+struct CampaignBannerCarouselViewObject: Identifiable {
+    let id: Int
+    let bannerContentsId: Int
+    let bannerUrl: URL?
+}
+
+// MARK: - Preview
 
 struct CampaignBannerCarouselView_Previews: PreviewProvider {
-    static var previews: some View {
-        CampaignBannerCarouselView()
-    }
-}
-
-extension CampaignBannerCarouselView_Previews {
     
+    static var previews: some View {
+        // MEMO: Preview表示用にレスポンスを想定したJsonを読み込んで画面に表示させる
+        let campaignBannersResponse = getCampaignBannersDummyResponse()
+        let campaignBannerCarouselViewObjects = campaignBannersResponse.result
+            .map {
+                CampaignBannerCarouselViewObject(
+                    id: $0.id,
+                    bannerContentsId: $0.bannerContentsId,
+                    bannerUrl: URL(string: $0.bannerUrl) ?? nil
+                )
+            }
+        CampaignBannerCarouselView(campaignBannersCarouselViewObjects: campaignBannerCarouselViewObjects)
+    }
+
+    // MARK: - Private Static Function
+
+    private static func getCampaignBannersDummyResponse() -> CampaignBannersResponse {
+        guard let path = Bundle.main.path(forResource: "campaign_banners", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let campaignBannersResponse = try? JSONDecoder().decode(CampaignBannersResponse.self, from: data) else {
+            fatalError()
+        }
+        return campaignBannersResponse
+    }
 }
--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Screens/HomeScreenView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Screens/HomeScreenView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct HomeScreenView: View {
-
+    
     // MARK: - body
-
+    
     var body: some View {
         NavigationView {
-            Text("HomeScreenView")
+            let campaignBannersResponse = getCampaignBannersDummyResponse()
+            let campaignBannerCarouselViewObjects = campaignBannersResponse.result
+                .map {
+                    CampaignBannerCarouselViewObject(
+                        id: $0.id,
+                        bannerContentsId: $0.bannerContentsId,
+                        bannerUrl: URL(string: $0.bannerUrl) ?? nil
+                    )
+                }
+            CampaignBannerCarouselView(campaignBannersCarouselViewObjects: campaignBannerCarouselViewObjects)
                 .navigationBarTitle(Text("Home"), displayMode: .inline)
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+
+    // MARK: - Private Static Function
+
+    private func getCampaignBannersDummyResponse() -> CampaignBannersResponse {
+        guard let path = Bundle.main.path(forResource: "campaign_banners", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let campaignBannersResponse = try? JSONDecoder().decode(CampaignBannersResponse.self, from: data) else {
+            fatalError()
+        }
+        return campaignBannersResponse
     }
 }
 
