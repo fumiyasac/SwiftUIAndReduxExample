6118456326eeecdf1f0064ff549e853cbeaba78c
fumiyasac <just1factory@gmail.com>
2022-12-26 17:33:32 +0900

    ProfileContentsViewの追加

---
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Profile/ProfileContentsView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileContentsView.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2022/12/26.
+//
+
+import SwiftUI
+
+struct ProfileContentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileContentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileContentsView()
+    }
+}
