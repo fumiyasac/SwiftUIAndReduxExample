1306ed916b322c7edf7772184cbc4816a71f21a5
fumiyasac <just1factory@gmail.com>
2023-01-05 23:56:52 +0900

    フリーワード入力用View要素を移植してみる

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Archive/SearchAreaComponent/ArchiveFreewordView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Archive/SearchAreaComponent/ArchiveFreewordView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct ArchiveFreewordView: View {
+
+    // MARK: - Property
+
+    private var searchBarBackgroundColor: Color {
+        return Color(uiColor: UIColor(code: "#e7e7e7"))
+    }
+
+    private var glassIconColor: Color {
+        return Color.gray
+    }
+
+    private var textFieldTextColor: Color {
+        return Color.primary
+    }
+
+    //
+    @Binding var text: String
+    @Binding var isLoading: Bool
+
+    //
+    @State private var isEditing = false
+
+    // MARK: - Body
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //
+        ZStack(alignment: .leading) {
+            searchBarBackgroundColor
+                .frame(width: 270.0)
+                .frame(height: 36.0)
+                .cornerRadius(8.0)
+            //
+            HStack {
+                //
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(glassIconColor)
+                    .padding([.leading], 8.0)
+                //
+                TextField("Search", text: $text)
+                    .padding(7.0)
+                    .padding(.leading, -8.0)
+                    .background(searchBarBackgroundColor)
+                    .cornerRadius(8.0)
+                    .foregroundColor(textFieldTextColor)
+                    .onTapGesture(perform: {
+                        isEditing = true
+                    })
+                
+                //
+                if isEditing {
+                    //
+                    Button(action: {
+                        //
+                        text = ""
+                        isEditing = false
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.gray)
+                    })
+                    .padding([.leading], 4.0)
+                    .padding([.trailing], 8.0)
+                    .transition(.move(edge: .trailing))
+                }
+            }
+        }
+        .padding([.leading, .trailing], 12.0)
     }
 }
 
+// MARK: - Preview
+
 struct ArchiveFreewordView_Previews: PreviewProvider {
     static var previews: some View {
-        ArchiveFreewordView()
+        ArchiveFreewordView(text: .constant(""), isLoading: .constant(false))
     }
 }
