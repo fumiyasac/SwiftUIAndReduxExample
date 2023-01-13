d87f39c12ef4faff55a742040fcbd877cc3ddbd2
fumiyasac <just1factory@gmail.com>
2023-01-13 23:52:37 +0900

    スクリーンの調整対応

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Screens/ArchiveScreenView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Screens/ArchiveScreenView.swift
@@ -13,11 +13,49 @@ struct ArchiveScreenView: View {
 
     var body: some View {
         NavigationView {
-            ArchiveSearchView()
+            ArchiveSearchView(archiveCellViewObjects: getArchiveCellViewObjects())
                 .navigationTitle("Archive")
                 .navigationBarTitleDisplayMode(.inline)
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+// MARK: ArchiveScreenView Extension
+
+extension ArchiveScreenView {
+    
+    // MARK: - Private Function
+
+    private func getArchiveCellViewObjects() -> [ArchiveCellViewObject] {
+
+        // MEMO: Preview表示用にレスポンスを想定したJsonを読み込んで画面に表示させる
+        let achiveSceneResponse = getArchiveSceneResponse()
+        let archiveCellViewObjects = achiveSceneResponse.result
+            .map {
+                ArchiveCellViewObject(
+                    id: $0.id,
+                    photoUrl: URL(string: $0.photoUrl) ?? nil,
+                    category: $0.category,
+                    dishName: $0.dishName,
+                    shopName: $0.shopName,
+                    introduction: $0.introduction
+                )
+            }
+        return archiveCellViewObjects
+    }
+
+    private func getArchiveSceneResponse() -> ArchiveSceneResponse {
+        guard let path = Bundle.main.path(forResource: "achive_images", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let achiveSceneResponse = try? JSONDecoder().decode(ArchiveSceneResponse.self, from: data) else {
+            fatalError()
+        }
+        return achiveSceneResponse
     }
 }
 
