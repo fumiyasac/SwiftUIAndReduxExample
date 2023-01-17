3da0fccf12989955ef3cf59eed1e9feb0e759749
fumiyasac <just1factory@gmail.com>
2023-01-18 07:53:22 +0900

    HomeResponseを取得する処理を記載する

---
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Model/DataTransfer/Response/HomeResponse.swift
@@ -0,0 +1,10 @@
+//
+//  HomeResponse.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2023/01/18.
+//
+
+import Foundation
+
+protocol HomeResponse {}
---
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/HomeRepository.swift
@@ -0,0 +1,58 @@
+//
+//  HomeRepository.swift
+//  SwiftUIAndReduxExample
+//
+//  Created by 酒井文也 on 2023/01/18.
+//
+
+import Foundation
+
+// MARK: - Protocol
+
+protocol HomeRepository {
+    func getHomeResponses() async throws -> [HomeResponse]
+}
+
+final class HomeRepositoryImpl: HomeRepository {
+
+    // MARK: - Function
+
+    func getHomeResponses() async throws -> [HomeResponse] {
+        var responses: [HomeResponse] = []
+        // 👉 エンドポイントの並び順を担保しながらの並列処理を実行する
+        try await withThrowingTaskGroup(of: HomeResponse.self, body: { group in
+            group.addTask {
+                return try await ApiClientManager.shared.getCampaignBanners()
+            }
+            group.addTask {
+                return try await ApiClientManager.shared.getRecentNews()
+            }
+            group.addTask {
+                return try await ApiClientManager.shared.getFeaturedTopics()
+            }
+            group.addTask {
+                return try await ApiClientManager.shared.getTrendArticles()
+            }
+            group.addTask {
+                return try await ApiClientManager.shared.getPickupPhotos()
+            }
+            // 👉 endpointsの配列に格納されている順番でレスポンスデータが格納される
+            for try await response in group {
+                responses.append(response)
+            }
+            // 👉 エラーハンドリング処理（例. ここではレスポンスが全て空だった場合はエラーとみなす）
+            if responses.isEmpty {
+                throw APIError.error(message: "All Response about Home is Empty.")
+            }
+        })
+        return responses
+    }
+}
+
+// MARK: - Factory
+
+struct HomeRepositoryFactory {
+    static func create() -> HomeRepository {
+        return HomeRepositoryImpl()
+    }
+}
