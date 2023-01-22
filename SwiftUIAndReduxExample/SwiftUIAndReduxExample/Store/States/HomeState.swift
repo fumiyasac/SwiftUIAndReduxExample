0dc2c143e709f957ab60c7d4f0819f720a15b577
fumiyasac <just1factory@gmail.com>
2023-01-23 08:01:50 +0900

    HomeStateの変更対応

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/Store/States/HomeState.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Store/States/HomeState.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 struct HomeState: ReduxState {
+    // MEMO: 読み込み中¥状態
+    var isLoading: Bool = false
+    // MEMO: エラー状態
+    var isError: Bool = false
+
     // MEMO: Home画面で利用する情報として必要なEntity情報
     // ※ このコードではEntityとView表示要素のComponentが1:1対応となる想定で作っています。
     var campaignBanners: [CampaignBannerEntity] = []
