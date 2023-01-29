860f1920cffb687794e35bc03ef2c46fa1f48bf1
fumiyasac <just1factory@gmail.com>
2023-01-29 20:36:06 +0900

    ProfileSocialMediaLinkViewとViewObjectを連結する

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Profile/Section/ProfileSocialMediaLink/ProfileSocialMediaLinkView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Profile/Section/ProfileSocialMediaLink/ProfileSocialMediaLinkView.swift
@@ -23,36 +23,53 @@ struct ProfileSocialMediaLinkView: View {
         return Color.secondary
     }
 
-    typealias SocialMediaColorSet = (name: String, color: Color)
+    private var profileSocialMediaViewObject: ProfileSocialMediaViewObject
 
-    private let socialMediaTitleAndColors: [SocialMediaColorSet] = [
-        (name: "Twitter", color: Color(uiColor: UIColor(code: "#00aced"))),
-        (name: "Facebook", color: Color(uiColor: UIColor(code: "#3b5998"))),
-        (name: "Instagram", color: Color(uiColor: UIColor(code: "#dd2a7b")))
-    ]
+    // MEMO: LazyVGridに表示する内容を格納するための変数
+    @State private var socialMediaSets: [SocialMediaSet] = []
+
+    // MARK: - Typealias
+
+    typealias SocialMediaSet = (name: String, color: Color, url: URL?)
 
     // MARK: - Initializer
 
-    init() {}
+    init(profileSocialMediaViewObject: ProfileSocialMediaViewObject) {
+        self.profileSocialMediaViewObject = profileSocialMediaViewObject
+
+        // イニシャライザ内で「_(変数名)」値を代入することでState値の初期化を実行する
+        _socialMediaSets = State(
+            initialValue: [
+                SocialMediaSet(name: "Twitter", color: Color(uiColor: UIColor(code: "#00aced")), url: profileSocialMediaViewObject.twitterUrl),
+                SocialMediaSet(name: "Facebook", color: Color(uiColor: UIColor(code: "#3b5998")), url: profileSocialMediaViewObject.facebookUrl),
+                SocialMediaSet(name: "Instagram", color: Color(uiColor: UIColor(code: "#dd2a7b")), url: profileSocialMediaViewObject.instagramUrl)
+            ]
+        )
+    }
 
     // MARK: - Body
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0.0) {
-            // TODO: 変数socialMediaTitleAndColorsとModelデータより取得した値を合わせて表示する
-            ForEach(0..<socialMediaTitleAndColors.count, id: \.self) { index in
+            // 変数socialMediaSetsより取得した値を合わせて表示する
+            ForEach(0..<socialMediaSets.count, id: \.self) { index in
                 HStack {
-                    Text(socialMediaTitleAndColors[index].name)
+                    Text(socialMediaSets[index].name)
                         .font(socialMediaTitleFont)
-                        .foregroundColor(socialMediaTitleAndColors[index].color)
+                        .foregroundColor(socialMediaSets[index].color)
                     Spacer()
-                    Link("URLを開く", destination: URL(string: "https://twitter.com/fumiyasac")!)
-                        .font(socialMediaLinkFont)
-                        .foregroundColor(socialMediaLinkColor)
-                    Image(systemName: "arrow.up.right.square")
-                        .font(socialMediaLinkFont)
-                        .foregroundColor(socialMediaLinkColor)
-                    
+                    if let url = socialMediaSets[index].url {
+                        Link("URLを開く", destination: url)
+                            .font(socialMediaLinkFont)
+                            .foregroundColor(socialMediaLinkColor)
+                        Image(systemName: "arrow.up.right.square")
+                            .font(socialMediaLinkFont)
+                            .foregroundColor(socialMediaLinkColor)
+                    } else {
+                        Text("No Link")
+                            .font(socialMediaLinkFont)
+                            .foregroundColor(socialMediaLinkColor)
+                    }
                 }
                 .padding([.leading, .trailing], 4.0)
                 .padding([.top, .bottom], 10.0)
@@ -66,6 +83,14 @@ struct ProfileSocialMediaLinkView: View {
 
 struct ProfileSocialMediaLinkView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileSocialMediaLinkView()
+        // MEMO: 部品1つあたりを表示するためのViewObject
+        let profileSocialMediaViewObject = ProfileSocialMediaViewObject(
+            id: 100,
+            twitterUrl: URL(string: "https://twitter.com/"),
+            facebookUrl: URL(string: "https://facebook.com/"),
+            instagramUrl: URL(string: "https://instagram.com/")
+        )
+        // Preview: ProfileSocialMediaLinkView
+        ProfileSocialMediaLinkView(profileSocialMediaViewObject: profileSocialMediaViewObject)
     }
 }
