0d1bead7fd9808dcbe35136fccefb0fb0fb5319b
fumiyasac <just1factory@gmail.com>
2023-01-29 20:34:38 +0900

    ProfilePersonalViewとViewObjectを連結する

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Profile/Section/ProfilePersonal/ProfilePersonalView.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/View/Components/Profile/Section/ProfilePersonal/ProfilePersonalView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ProfilePersonalView: View {
 
@@ -27,11 +28,11 @@ struct ProfilePersonalView: View {
         return Color.gray
     }
 
-    private var personLastLoginFont: Font {
+    private var personIdFont: Font {
         return Font.custom("AvenirNext-Regular", size: 12)
     }
 
-    private var personLastLoginColor: Color {
+    private var personIdColor: Color {
         return Color.gray
     }
 
@@ -39,9 +40,13 @@ struct ProfilePersonalView: View {
         return 86.0
     }
 
+    private var profilePersonalViewObject: ProfilePersonalViewObject
+
     // MARK: - Initializer
 
-    init() {}
+    init(profilePersonalViewObject: ProfilePersonalViewObject) {
+        self.profilePersonalViewObject = profilePersonalViewObject
+    }
 
     // MARK: - Body
 
@@ -49,7 +54,7 @@ struct ProfilePersonalView: View {
         VStack(alignment: .leading, spacing: 0.0) {
             HStack {
                 // 1. プロフィール用アバター表示
-                Image("profile_avatar_sample")
+                KFImage(profilePersonalViewObject.avatarUrl)
                     .resizable()
                     .scaledToFill()
                     .clipShape(Circle())
@@ -58,18 +63,18 @@ struct ProfilePersonalView: View {
                 // 2. プロフィール用基本情報表示
                 VStack(alignment: .leading) {
                     // 2-(1). ユーザー名表示
-                    Text("料理人●●●●")
+                    Text(profilePersonalViewObject.nickname)
                         .font(personNameFont)
                         .foregroundColor(personNameColor)
                     // 2-(2). ユーザー登録日表示
-                    Text("登録日: 2022.12.01")
+                    Text(profilePersonalViewObject.createdAt)
                         .font(personRegistrationFont)
                         .foregroundColor(personRegistrationColor)
                         .padding([.top], -8.0)
                     // 2-(3). ユーザー最終ログイン日時表示
-                    Text("最終ログイン: 2日前")
-                        .font(personLastLoginFont)
-                        .foregroundColor(personLastLoginColor)
+                    Text("ユーザーID: \(profilePersonalViewObject.id)")
+                        .font(personIdFont)
+                        .foregroundColor(personIdColor)
                         .padding([.top], -8.0)
                 }
                 .padding([.leading], 8.0)
@@ -85,6 +90,14 @@ struct ProfilePersonalView: View {
 
 struct ProfilePersonalView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfilePersonalView()
+        // MEMO: 部品1つあたりを表示するためのViewObject
+        let profilePersonalViewObject = ProfilePersonalViewObject(
+            id: 100,
+            nickname: "謎多き料理人",
+            createdAt: DateLabelFormatter.getDateStringFromAPI(apiDateString: "2022-11-16T07:30:00.000+0000"),
+            avatarUrl: URL(string: "https://ones-mind-topics.s3.ap-northeast-1.amazonaws.com/profile_avatar_sample.jpg")
+        )
+        // Preview: ProfilePersonalView
+        ProfilePersonalView(profilePersonalViewObject: profilePersonalViewObject)
     }
 }
