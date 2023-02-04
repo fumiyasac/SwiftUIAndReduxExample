4629671dbd8c80b34b5f8501cd8f9917548ee512
fumiyasac <just1factory@gmail.com>
2023-02-04 16:37:26 +0900

    RequestArchiveRepositoryのAPIリクエスト処理部分の新規作成

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// MARK: - Protocol
+
+protocol RequestArchiveRepository {
+    func getArchiveResponse(keyword: String, category: String) async throws -> ArchiveResponse
+}
+
+final class RequestArchiveRepositoryImpl: RequestArchiveRepository {
+
+    // MARK: - Function
+
+    func getArchiveResponse(keyword: String, category: String) async throws -> ArchiveResponse {
+        return try await ApiClientManager.shared.getAchiveImages(keyword: keyword, category: category)
+    }
+}
+
+// MARK: - Factory
+
+struct RequestArchiveRepositoryFactory {
+    static func create() -> RequestArchiveRepository {
+        return RequestArchiveRepositoryImpl()
+    }
+}
