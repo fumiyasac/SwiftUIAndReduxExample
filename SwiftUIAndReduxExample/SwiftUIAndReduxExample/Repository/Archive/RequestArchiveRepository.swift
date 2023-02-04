71658d4f529002a55ac347d67f3224c2046c31b9
fumiyasac <just1factory@gmail.com>
2023-02-04 16:54:39 +0900

    MockSuccessRequestArchiveRepositoryImplの新規追加

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
@@ -22,10 +22,45 @@ final class RequestArchiveRepositoryImpl: RequestArchiveRepository {
     }
 }
 
+// MARK: - MockSuccessRequestArchiveRepositoryImpl
+
+final class MockSuccessRequestArchiveRepositoryImpl: RequestArchiveRepository {
+
+    // MARK: - Function
+
+    func getArchiveResponse(keyword: String, category: String) async throws -> ArchiveResponse {
+        let filteredResult = getArchiveSceneResponse().result
+            .filter { $0.dishName.contains(keyword) || $0.shopName.contains(keyword)  || $0.introduction.contains(keyword) }
+            .filter { $0.category == category }
+        return ArchiveSceneResponse(result: filteredResult)
+    }
+
+    // MARK: - Private Function
+
+    private func getArchiveSceneResponse() -> ArchiveSceneResponse {
+        guard let path = Bundle.main.path(forResource: "achive_images", ofType: "json") else {
+            fatalError()
+        }
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+        guard let result = try? JSONDecoder().decode([ArchiveSceneEntity].self, from: data) else {
+            fatalError()
+        }
+        return ArchiveSceneResponse(result: result)
+    }
+}
+
 // MARK: - Factory
 
 struct RequestArchiveRepositoryFactory {
     static func create() -> RequestArchiveRepository {
         return RequestArchiveRepositoryImpl()
+    }
+}
+
+struct MockSuccessRequestArchiveRepositoryFactory {
+    static func create() -> RequestArchiveRepository {
+        return MockSuccessRequestArchiveRepositoryImpl()
     }
 }
