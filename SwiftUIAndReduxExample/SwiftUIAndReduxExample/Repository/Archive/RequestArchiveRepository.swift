e051cf93284129e54ea9c95834bbcd2664e839f2
fumiyasac <just1factory@gmail.com>
2023-02-04 17:04:40 +0900

    補足コメントの追記対応

--- SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExample/Repository/Archive/RequestArchiveRepository.swift
@@ -29,9 +29,12 @@ final class MockSuccessRequestArchiveRepositoryImpl: RequestArchiveRepository {
     // MARK: - Function
 
     func getArchiveResponse(keyword: String, category: String) async throws -> ArchiveResponse {
+        // 👉 実際にAPIリクエストで発生する処理に近しいものをMockで再現する
+        // 第2引数で与えられるcategoryと全く同じ値であるものだけを取り出す
+        // 第1引数で与えられるkeywordが(dishName / shopName / introduction)いずれかに含まれるものだけを取り出す
         let filteredResult = getArchiveSceneResponse().result
-            .filter { $0.dishName.contains(keyword) || $0.shopName.contains(keyword)  || $0.introduction.contains(keyword) }
             .filter { $0.category == category }
+            .filter { $0.dishName.contains(keyword) || $0.shopName.contains(keyword)  || $0.introduction.contains(keyword) }
         return ArchiveSceneResponse(result: filteredResult)
     }
 
