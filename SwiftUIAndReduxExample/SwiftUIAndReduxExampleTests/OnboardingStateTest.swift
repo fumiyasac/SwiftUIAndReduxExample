58e457394246e45750af6039068613501d88578f
fumiyasac <just1factory@gmail.com>
2023-02-07 00:49:07 +0900

    お試しでOnboardingStateの部分のテストコードを追加してみる

--- SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/OnboardingStateTest.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/OnboardingStateTest.swift
@@ -6,16 +6,20 @@
 //
 
 @testable import SwiftUIAndReduxExample
-import Nimble
-import Quick
+
 import XCTest
+import Combine
 
 final class OnboardingStateTest: XCTestCase {
+
+    // stateの格納先が @Published なので購読のキャンセルができる様にしておく
+    private var cancellables: [AnyCancellable] = []
 
     // MARK: - Function (test_showOnboardingIsTrue)
 
     func test_showOnboardingIsTrue() throws {
-        // MEMO: Mock用のMiddlewareを適用したStoreを用意する（テスト対象のMiddlewareだけで差し支えはない）
+        // MEMO: Mock用のMiddlewareを適用したStoreを用意する
+        // 👉 ①テスト対象のMiddlewareだけの状態でOKです
         let store = Store(
             reducer: appReducer,
             state: AppState(),
@@ -24,14 +28,19 @@ final class OnboardingStateTest: XCTestCase {
                 onboardingMockCloseMiddleware()
             ]
         )
+        // MEMO: テストの書き方は下記リンクを参考にしました。
+        // https://stackoverflow.com/questions/59690913/how-do-i-properly-test-a-var-that-changes-through-a-publisher-in-my-viewmodel-in
         let expectationshowOnboardingTrue = self.expectation(description: "Expect showOnboarding is true.")
-        let subscriber = store.$state.sink(receiveValue: { state in
+        let _ = store.$state.sink(receiveValue: { state in
             if state.onboardingState.showOnboarding == true {
+                // 👉 ②対象のStateに変化が起こった場合にfulfill()を実行する
                 expectationshowOnboardingTrue.fulfill()
             }
-        })
+        }).store(in: &cancellables)
+        // 👉 ③該当StateのPropertyを変更するためのActionを発行する
         store.dispatch(action: RequestOnboardingAction())
         waitForExpectations(timeout: 1.0, handler: { _ in
+            // 👉 ④fulfill()を実行後には対象のState内のPropetyが想定した値へ変更されていることを確認する
             let onboardingState = store.state.onboardingState
             XCTAssertEqual(true, onboardingState.showOnboarding)
         })
