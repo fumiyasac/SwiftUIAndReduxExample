c17e55840c4fd70461c86d14ab9f8a6f84951db7
fumiyasac <just1factory@gmail.com>
2023-02-07 22:16:55 +0900

    HomeStateのテスト例を記載する対応

--- SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/HomeStateTest.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/HomeStateTest.swift
@@ -20,8 +20,8 @@ final class HomeStateTest: XCTestCase {
 
     // MARK: - Function (test_SuccessHomeResponse)
 
-    // 👉 取得したレスポンスがHomeState内のPropertyに反映されることを確認する
-    func test_SuccessHomeResponse() throws {
+    // 👉 取得したレスポンスがHomeState内のPropertyに反映されることを確認する(CampaignBannerCarouselViewObjectの確認)
+    func test_SuccessHomeResponse_CampaignBannerCarouselViewObjects() throws {
         // MEMO: Mock用のMiddlewareを適用したStoreを用意する
         let store = Store(
             reducer: appReducer,
@@ -30,5 +30,56 @@ final class HomeStateTest: XCTestCase {
                 homeMockSuccessMiddleware()
             ]
         )
+        // MEMO: Storeから取得できたデータを格納するための変数
+        var targetCampaignBannerCarouselViewObjects: [CampaignBannerCarouselViewObject] = []
+        // MEMO: テスト前状態のState値を作る
+        let beforeTestState = store.state
+        // MEMO: Combineの処理を利用した形でActionが発行された場合での
+        let expectationHomeSuccess = self.expectation(description: "Expect to get CampaignBannerCarouselViewObjects.")
+        let _ = store.$state.sink(receiveValue: { changedState in
+            if beforeTestState.homeState.campaignBannerCarouselViewObjects != changedState.homeState.campaignBannerCarouselViewObjects {
+                targetCampaignBannerCarouselViewObjects = changedState.homeState.campaignBannerCarouselViewObjects
+                expectationHomeSuccess.fulfill()
+            }
+        }).store(in: &cancellables)
+        store.dispatch(action: RequestHomeAction())
+        waitForExpectations(timeout: 2.0, handler: { _ in
+            // Example: 総数と最初の内容ぐらいは確認しておくと良さそうに思います。
+            XCTAssertEqual(6, targetCampaignBannerCarouselViewObjects.count, "季節の特集コンテンツ一覧は合計6件取得できること")
+            let firstViewObject = targetCampaignBannerCarouselViewObjects.first
+            XCTAssertEqual(1, firstViewObject?.id, "1番目のidが正しい値であること")
+            XCTAssertEqual(1001, firstViewObject?.bannerContentsId, "1番目のbannerContentsIdが正しい値であること")
+        })
+    }
+
+    // 👉 取得したレスポンスがHomeState内のPropertyに反映されることを確認する(RecentNewsCarouselViewObjectの確認)
+    func test_SuccessHomeResponse_RecentNewsCarouselViewObjects() throws {
+        // MEMO: Mock用のMiddlewareを適用したStoreを用意する
+        let store = Store(
+            reducer: appReducer,
+            state: AppState(),
+            middlewares: [
+                homeMockSuccessMiddleware()
+            ]
+        )
+        // MEMO: Storeから取得できたデータを格納するための変数
+        var targetRecentNewsCarouselViewObjects: [RecentNewsCarouselViewObject] = []
+        // MEMO: テスト前状態のState値を作る
+        let beforeTestState = store.state
+        // MEMO: Combineの処理を利用した形でActionが発行された場合での
+        let expectationHomeSuccess = self.expectation(description: "Expect to get RecentNewsCarouselViewObjects.")
+        let _ = store.$state.sink(receiveValue: { changedState in
+            if beforeTestState.homeState.recentNewsCarouselViewObjects != changedState.homeState.recentNewsCarouselViewObjects {
+                targetRecentNewsCarouselViewObjects = changedState.homeState.recentNewsCarouselViewObjects
+                expectationHomeSuccess.fulfill()
+            }
+        }).store(in: &cancellables)
+        store.dispatch(action: RequestHomeAction())
+        waitForExpectations(timeout: 2.0, handler: { _ in
+            XCTAssertEqual(12, targetRecentNewsCarouselViewObjects.count, "最新のお知らせは合計12件取得できること")
+            let lastViewObject = targetRecentNewsCarouselViewObjects.last
+            XCTAssertEqual(12, lastViewObject?.id, "最後のidが正しい値であること")
+            XCTAssertEqual("美味しいみかんの年末年始の対応について", lastViewObject?.title, "最後のtitleが正しい値であること")
+        })
     }
 }
