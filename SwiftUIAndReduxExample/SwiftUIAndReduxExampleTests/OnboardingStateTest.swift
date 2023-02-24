cb4e3dfd9d20ce65fd95749399a02c150cebf3b8
fumiyasac <just1factory@gmail.com>
2023-02-24 09:01:03 +0900

    UnitTestを新しい形にしたので、既存のテストコードを削除する

--- SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/OnboardingStateTest.swift
+++ SwiftUIAndReduxExample/SwiftUIAndReduxExampleTests/OnboardingStateTest.swift
@@ -20,7 +20,7 @@ import Quick
 // https://github.com/groue/CombineExpectations#usage
 
 
-final class OnboardingStateChangeTest: QuickSpec {
+final class OnboardingStateTest: QuickSpec {
 
     // MARK: - Override
 
@@ -67,42 +67,3 @@ final class OnboardingStateChangeTest: QuickSpec {
     }
 }
 
-final class OnboardingStateTest: XCTestCase {
-
-    // stateの格納先が @Published なので購読のキャンセルができる様にしておく
-    private var cancellables: [AnyCancellable] = []
-
-    // MARK: - Function (test_showOnboardingIsTrue)
-
-    func test_showOnboardingIsTrue() throws {
-        // MEMO: Mock用のMiddlewareを適用したStoreを用意する
-        // 👉 ①テスト対象のMiddlewareだけの状態でOKです
-        let store = Store(
-            reducer: appReducer,
-            state: AppState(),
-            middlewares: [
-                onboardingMockShowMiddleware(),
-                onboardingMockCloseMiddleware()
-            ]
-        )
-        // MEMO: Storeから取得できたデータを格納するための変数
-        var targetTestShowOnboarding: Bool?
-        // MEMO: テスト前状態のState値を作る
-        let beforeTestState = store.state
-        // MEMO: Combineの処理を利用した形でActionが発行された場合での値変化を監視する
-        let expectationshowOnboardingTrue = self.expectation(description: "Expect showOnboarding is true.")
-        let _ = store.$state.sink(receiveValue: { changedState in
-            // 👉 ②対象のState内Propertyに変化が起こった場合にfulfill()を実行する
-            if beforeTestState.onboardingState.showOnboarding != changedState.onboardingState.showOnboarding {
-                targetTestShowOnboarding = changedState.onboardingState.showOnboarding
-                expectationshowOnboardingTrue.fulfill()
-            }
-        }).store(in: &cancellables)
-        // 👉 ③該当StateのPropertyを変更するためのActionを発行する
-        store.dispatch(action: RequestOnboardingAction())
-        waitForExpectations(timeout: 1.0, handler: { _ in
-            // 👉 ④fulfill()を実行後には対象のState内のPropetyが想定した値へ変更されていることを確認する
-            XCTAssertEqual(true, targetTestShowOnboarding)
-        })
-    }
-}
